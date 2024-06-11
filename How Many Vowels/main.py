509baf6b4240c8fb5d329835a826cac1a66d7b43
Brandon <brandonbb99@gmail.com>
2024-06-12 01:46:41 +0800

    Created function to count vowels

--- How Many Vowels/main.py
+++ How Many Vowels/main.py
@@ -1,1 +1,15 @@
-print("hello")
+
+
+def countVowels(word):
+    vowels = ['a','e','i','o','u']
+    word_lower = word.lower()
+    vowelsCounter = 0
+    for letter in word_lower:
+        if letter in vowels:
+            vowelsCounter +=1
+    return vowelsCounter
+
+
+if __name__== "__main__":
+    print(countVowels("ASD"))
+    
